733d9faccafe8acdd31297537a4546cf681e7da3
nirmitp <nirmitpatel1994@gmail.com>
2016-11-29 14:21:28 -0800

    First Game Commit

---
+++ Assets/_CompletedAssets/Scripts/menuScript.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class menuScript : MonoBehaviour {
+
+    public Canvas quitMenu;
+    public Button startText;
+    public Button exitText;
+    public Text highscoreText;
+
+	// Use this for initialization
+	void Start () {
+        quitMenu = quitMenu.GetComponent<Canvas>();
+        startText = startText.GetComponent<Button>();
+        exitText = exitText.GetComponent<Button>();
+        quitMenu.enabled = false;
+        highscoreText.text = "Highscore : " + PlayerPrefs.GetInt("Highscore");
+    }
+	
+    public void ExitPress ()
+    {
+        quitMenu.enabled = true;
+        startText.enabled = false;
+        exitText.enabled = false;
+    }
+
+    public void NoPress ()
+    {
+        quitMenu.enabled = false;
+        startText.enabled = true;
+        exitText.enabled = true;
+    }
+
+    public void StartLevel()
+    {
+        SceneManager.LoadScene("Level 01 5.x"); 
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
